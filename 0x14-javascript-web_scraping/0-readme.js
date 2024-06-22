f6dc70d507bd525152481938fb419057a6070dde
mikitseg <tsegayemikiyas9@gmail.com>
2024-06-23 01:14:53 +0300

    javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
+const fileName = process.argv[2];
 const fs = require('fs');
 
-// Read the contents of a file specified as a command-line argument
-// 'utf8' specifies the encoding of the file being read
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    // Log an error message if an error occurs during the file read operation
-    console.error('Error reading the file:', error);
-  } else {
-    // Log the contents of the file if it is read successfully
-    console.log(content);
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
   }
+  console.log(data);
 });
